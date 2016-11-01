8ab2a911fb509936e8ba073c90a4f457a2626856
Dallin Crane <dallincrane@gmail.com>
2016-11-01 17:53:06 -0600

    upgrade decimal filter and add spec

--- lib/chaotic.rb
+++ lib/chaotic.rb
@@ -6,6 +6,7 @@ require 'bigdecimal/util'
 
 require 'active_support/concern'
 require 'active_support/core_ext/object/try'
+require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/core_ext/hash/deep_merge'
 require 'active_support/core_ext/array/wrap'
---
+++ spec/filters/decimal_filter_spec.rb
@@ -0,0 +1,136 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe 'Chaotic::Filters::DecimalFilter' do
+  it 'allows bigdecimals' do
+    f = Chaotic::Filters::DecimalFilter.new
+    filtered, errors = f.filter(BigDecimal.new('0.99999999999999999'))
+
+    assert filtered.is_a?(BigDecimal)
+    assert_equal '0.99999999999999999', filtered.to_s('F')
+    assert_equal nil, errors
+  end
+
+  it 'allows floats' do
+    f = Chaotic::Filters::DecimalFilter.new
+    filtered, errors = f.filter(3.1415926)
+
+    assert filtered.is_a?(BigDecimal)
+    assert_equal 3.1415926, filtered
+    assert_equal nil, errors
+  end
+
+  it 'allows strings that start with a digit' do
+    f = Chaotic::Filters::DecimalFilter.new
+    filtered, errors = f.filter('3')
+
+    assert filtered.is_a?(BigDecimal)
+    assert_equal 3.0, filtered
+    assert_equal nil, errors
+  end
+
+  it 'allows string representation of float' do
+    f = Chaotic::Filters::DecimalFilter.new
+    filtered, errors = f.filter('3.14')
+
+    assert filtered.is_a?(BigDecimal)
+    assert_equal 3.14, filtered
+    assert_equal nil, errors
+  end
+
+  it 'allows string representation of float without a number before dot' do
+    f = Chaotic::Filters::DecimalFilter.new
+    filtered, errors = f.filter('.14')
+
+    assert filtered.is_a?(BigDecimal)
+    assert_equal 0.14, filtered
+    assert_equal nil, errors
+  end
+
+  it 'allows negative strings' do
+    f = Chaotic::Filters::DecimalFilter.new
+    filtered, errors = f.filter('-.14')
+
+    assert filtered.is_a?(BigDecimal)
+    assert_equal(-0.14, filtered)
+    assert_equal nil, errors
+  end
+
+  it 'allows strings with a positive sign' do
+    f = Chaotic::Filters::DecimalFilter.new
+    filtered, errors = f.filter('+.14')
+
+    assert filtered.is_a?(BigDecimal)
+    assert_equal 0.14, filtered
+    assert_equal nil, errors
+  end
+
+  it 'does not allow other strings, nor does it allow random objects or symbols' do
+    f = Chaotic::Filters::DecimalFilter.new
+
+    ['zero', 'a1', {}, [], Object.new, :d].each do |thing|
+      _filtered, errors = f.filter(thing)
+
+      assert_equal :decimal, errors
+    end
+  end
+
+  it 'considers nil to be invalid' do
+    f = Chaotic::Filters::DecimalFilter.new(:x, nils: false)
+    filtered, errors = f.filter(nil)
+
+    assert_equal nil, filtered
+    assert_equal :nils, errors
+  end
+
+  it 'considers nil to be valid' do
+    f = Chaotic::Filters::DecimalFilter.new(:x, nils: true)
+    filtered, errors = f.filter(nil)
+
+    assert_equal nil, filtered
+    assert_equal nil, errors
+  end
+
+  it 'considers empty strings to be invalid' do
+    f = Chaotic::Filters::DecimalFilter.new
+    _filtered, errors = f.filter('')
+
+    assert_equal :decimal, errors
+  end
+
+  it 'considers low numbers invalid' do
+    f = Chaotic::Filters::DecimalFilter.new(:x, min: 10)
+    filtered, errors = f.filter(3)
+
+    assert filtered.is_a?(BigDecimal)
+    assert_equal 3, filtered
+    assert_equal :min, errors
+  end
+
+  it 'considers low numbers valid' do
+    f = Chaotic::Filters::DecimalFilter.new(:x, min: 10)
+    filtered, errors = f.filter(31)
+
+    assert filtered.is_a?(BigDecimal)
+    assert_equal 31, filtered
+    assert_equal nil, errors
+  end
+
+  it 'considers high numbers invalid' do
+    f = Chaotic::Filters::DecimalFilter.new(:x, max: 10)
+    filtered, errors = f.filter(31)
+
+    assert filtered.is_a?(BigDecimal)
+    assert_equal 31, filtered
+    assert_equal :max, errors
+  end
+
+  it 'considers high numbers vaild' do
+    f = Chaotic::Filters::DecimalFilter.new(:x, max: 10)
+    filtered, errors = f.filter(3)
+
+    assert filtered.is_a?(BigDecimal)
+    assert_equal 3, filtered
+    assert_equal nil, errors
+  end
+end
