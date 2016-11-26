7b81f3c21b7d3f5143a133ee3baad68bf46083ad
Dallin Crane <dallincrane@gmail.com>
2016-11-25 23:00:45 -0700

    update duck filter

--- lib/chaotic/filters/duck_filter.rb
+++ lib/chaotic/filters/duck_filter.rb
@@ -7,14 +7,33 @@ module Chaotic
         methods: nil
       )
 
-      def feed(data)
-        return handle_nil if data.nil?
+      # def feed(data)
+      #   return handle_nil if data.nil?
+      #
+      #   Array.wrap(options[:methods]).each do |method|
+      #     return [data, :duck] unless data.respond_to?(method)
+      #   end
+      #
+      #   [data, nil]
+      # end
 
-        Array.wrap(options[:methods]).each do |method|
-          return [data, :duck] unless data.respond_to?(method)
-        end
+      def feed(given)
+        return handle_nil if given.nil?
 
-        [data, nil]
+        coerced = coerce(given)
+        return [given, :duck] unless respond_to_all?(coerced)
+
+        [coerced, nil]
+      end
+
+      def coerce(given)
+        given
+      end
+
+      def respond_to_all?(coerced)
+        Array.wrap(options[:methods]).map do |method|
+          coerced.respond_to?(method)
+        end.all?
       end
     end
   end
